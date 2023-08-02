2894efba2afc75fa3180b1c6cf1debe8a2035a8b
CMD-God <48065803+CMD-God@users.noreply.github.com>
2023-08-02 17:10:53 +0300

    Work on login page

---
+++ js/login.js
@@ -0,0 +1,18 @@
+
+function addNewUser(name, avatar = {image: "Dark_Destiny", rarity: "MYTHIC"}, loginStatus = false) {
+    var cont = document.createElement("DIV");
+    cont.className = "savedUserLine";
+    cont.innerHTML = `
+        <div class="fullRowFlex">
+        <img src="img/pfp/${avatar.image}.png" class="avatar ${avatar.rarity}">
+        <div class="savedUserText">
+            <div class="username">${name}</div>
+            <div class="loginStatus">${loginStatus ? "Logged in" : "<span class='red'>Not logged in</span>"}</div>
+        </div>
+    `;
+    document.getElementsByClassName("loginContainer")[0].appendChild(cont);
+}
+
+addNewUser("CMD_God");
+addNewUser("Angy-Ralsei", {image: "Delete_This_Ralsei", rarity: "TOKEN"}, true);
+addNewUser("Stick Rick", {image: "He_Never_Gave_Up", rarity: "EPIC"}, false);
